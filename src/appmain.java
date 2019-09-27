da0bf080ff47d235afd4eba556180d14d6cf2eaf
bohdan rosolovskiy <rosolovkiy@gmail.com>
2019-09-28 00:42:39 +0300

    Initial commit

---
+++ src/appmain.java
@@ -0,0 +1,22 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class appmain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        var a = 15;
+        var b = 1.6;
+        var c = 8;
+        var d = 13;
+        var e = 14;
+        System.out.println((a+b)/(d-b)*2);
+        System.out.println(a+2*2-12/a*(b-c));
+        System.out.println(b+11*c-a);
+        System.out.println(a*(c+b+d)-e/1+a);
+        System.out.println(a+b*(a-b)/12);
+
+        }
+
+
+    }
+
+
